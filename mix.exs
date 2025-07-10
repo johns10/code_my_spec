8418d22cd6d4262916cec1cb6bb9accf05f40bb6
John Davenport <johns10@gmail.com>
2025-07-10 13:33:07 -0400

    deps

--- mix.exs
+++ mix.exs
@@ -58,7 +58,11 @@ defmodule CodeMySpec.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:mox, "~> 1.2"}
+      {:mox, "~> 1.2"},
+      {:paper_trail, "~> 1.1.2"},
+      {:mcp_sse, "~> 0.1.6"},
+      {:oban, "~> 2.19.4"},
+      {:briefly, "~> 0.5.1"}
     ]
   end
 
