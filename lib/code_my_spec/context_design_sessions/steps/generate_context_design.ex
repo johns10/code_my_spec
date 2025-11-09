2504fa09782405adf6fedd39c910168bf1e9c505
John Davenport <johns10@gmail.com>
2025-11-09 00:12:36 -0500

    Include similar components in context prompt

--- lib/code_my_spec/context_design_sessions/steps/generate_context_design.ex
+++ lib/code_my_spec/context_design_sessions/steps/generate_context_design.ex
@@ -1,14 +1,16 @@
 defmodule CodeMySpec.ContextDesignSessions.Steps.GenerateContextDesign do
   @behaviour CodeMySpec.Sessions.StepBehaviour
 
-  alias CodeMySpec.{Rules, Utils, Stories}
+  alias CodeMySpec.{Rules, Utils, Stories, Components}
   alias CodeMySpec.Sessions.{Session, Steps.Helpers}
   alias CodeMySpec.Documents.{ContextDesign, DocumentSpecProjector}
 
   def get_command(scope, %Session{project: project, component: component}, opts \\ []) do
     with {:ok, rules} <- get_design_rules(scope),
+         similar_components <- Components.list_similar_components(scope, component),
          stories <- Stories.list_component_stories(scope, component.id),
-         {:ok, prompt} <- build_design_prompt(project, component, rules, stories),
+         {:ok, prompt} <-
+           build_design_prompt(project, component, rules, stories, similar_components),
          {:ok, command} <-
            Helpers.build_agent_command(
              __MODULE__,
@@ -32,10 +34,11 @@ defmodule CodeMySpec.ContextDesignSessions.Steps.GenerateContextDesign do
     end
   end
 
-  defp build_design_prompt(project, context, rules, stories) do
+  defp build_design_prompt(project, context, rules, stories, similar_components) do
     rules_text = Enum.map_join(rules, "\n\n", & &1.content)
     stories_text = format_stories(stories)
-    document_spec = DocumentSpecProjector.project_spec(ContextDesign)
+    similar_text = format_similar_components(project, similar_components)
+    document_spec = DocumentSpecProjector.project_spec(:context)
     %{design_file: design_file_path} = Utils.component_files(context, project)
 
     prompt = """
@@ -50,6 +53,9 @@ defmodule CodeMySpec.ContextDesignSessions.Steps.GenerateContextDesign do
     User Stories this context satisfies:
     #{stories_text}
 
+    Similar Components (for design inspiration):
+    #{similar_text}
+
     Document Specification:
     #{document_spec}
 
@@ -60,6 +66,25 @@ defmodule CodeMySpec.ContextDesignSessions.Steps.GenerateContextDesign do
     """
 
     {:ok, prompt}
+  end
+
+  defp format_similar_components(_project, []), do: "No similar components provided"
+
+  defp format_similar_components(project, similar_components) do
+    similar_components
+    |> Enum.with_index(1)
+    |> Enum.map_join("\n\n", fn {component, index} ->
+      files = Utils.component_files(component, project)
+
+      """
+      #{index}. #{component.name} (#{component.type})
+         Description: #{component.description || "No description"}
+         Design: #{files.design_file}
+         Implementation: #{files.code_file}
+         Test: #{files.test_file}
+      """
+      |> String.trim()
+    end)
   end
 
   defp format_stories([]), do: "No user stories provided"
