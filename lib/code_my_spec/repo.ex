5fad5e08e44311eae7479d8d6ba0702eb672919c
John Davenport <johns10@gmail.com>
2025-11-24 22:37:58 -0500

    conditional repo adapter

--- lib/code_my_spec/repo.ex
+++ lib/code_my_spec/repo.ex
@@ -1,5 +1,7 @@
 defmodule CodeMySpec.Repo do
   use Ecto.Repo,
     otp_app: :code_my_spec,
-    adapter: Ecto.Adapters.Postgres
+    adapter:
+      Application.compile_env(:code_my_spec, :adapter, Ecto.Adapters.Postgres)
+      |> IO.inspect(label: :repo_used)
 end
