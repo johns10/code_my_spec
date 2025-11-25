d0c63da461313ae7a61352b2e0691d1983308e43
John Davenport <johns10@gmail.com>
2025-11-24 22:40:21 -0500

    Fix inspect

--- lib/code_my_spec/repo.ex
+++ lib/code_my_spec/repo.ex
@@ -1,7 +1,5 @@
 defmodule CodeMySpec.Repo do
   use Ecto.Repo,
     otp_app: :code_my_spec,
-    adapter:
-      Application.compile_env(:code_my_spec, :adapter, Ecto.Adapters.Postgres)
-      |> IO.inspect(label: :repo_used)
+    adapter: Application.compile_env(:code_my_spec, :adapter, Ecto.Adapters.Postgres)
 end
